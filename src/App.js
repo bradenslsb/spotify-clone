649e331fdf5d8ae6135e6bd2eb3e3d904eed679f
sirhackalot <user123@users-MacBook-Pro.local>
2021-06-05 21:05:47 -0600

    Set up the login funcitonallity

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify"
+import SpotifyWebApi from "spotify-web-api-js"
+import Player from "./Player"
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        console.log("person", user)
+      })
+    }
+
+
+    console.log("I have a token" , token)
+  }, []);
+
+
   return (
     <div className="app">
-      <h2>Hello</h2>
-
-      {/*Sptify login
-        login with spotify button
-         */}
-      
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
       <Login />
     </div>
   );

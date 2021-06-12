f48ca845eeafc210bce8545a4eb76f45a41eb819
Bird <bbir1199@gmail.com>
2021-06-11 18:44:42 -0600

    built the basics of the ui

--- src/Player.js
+++ src/Player.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import "./Player.css"
 import Sidebar from "./Sidebar"
+import Body from "./Body"
+import Footer from "./Footer"
 
 function Player({ spotify }) {
   return (
     <div className="player">
       <div className="player_body">
         <Sidebar />
+        <Body />
 
       </div>
-      <div className="footer">
+      <div>
+        <Footer />
 
       </div>
-      <h1>welcome to spotify</h1>
+      
     </div>
   )
 }

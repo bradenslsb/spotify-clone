f6e3680cf8c4cc0c1ffb86d46f357dd53df77c37
Bird <bbir1199@gmail.com>
2021-06-15 11:45:27 -0600

    started on the body

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from "./Header"
 import "./Body.css"
 
-function Body() {
+function Body({ spotify }) {
   return (
     <div className="body">
       <h1>this is the body</h1>
+      <Header spotify={spotify} />
     </div>
   )
 }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-left">
+
+      </div>
+      <div className="header-right">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
